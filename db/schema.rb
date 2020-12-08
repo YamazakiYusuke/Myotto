6a3311536aa262e7a9702cd44752d47dad9e4065
YamazakiYusuke <yusuke38098@gmail.com>
2020-12-08 11:28:15 +0900

    bookテーブルにuser_id(FK)を追加 make_sentences_from_bookメソッドを少し変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_141318) do
+ActiveRecord::Schema.define(version: 2020_12_08_013911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2020_12_07_141318) do
     t.integer "language", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_books_on_user_id"
   end
 
   create_table "sentences", force: :cascade do |t|
@@ -51,5 +53,6 @@ ActiveRecord::Schema.define(version: 2020_12_07_141318) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
   add_foreign_key "sentences", "books"
 end
