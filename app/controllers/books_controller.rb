cefd217b472e7b69f74950193abb87562e55cc94
YamazakiYusuke <yusuke38098@gmail.com>
2020-12-08 13:22:32 +0900

    fix
    book#create でbookテーブルのuser_idを登録されるように修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,7 +13,7 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new(book_params)
+    @book =  current_user.books.new(book_params)
     if params[:back]
       render :new
     else
