9eb86d3a32bd84dd7272b6ae643512087c246a87
YamazakiYusuke <yusuke38098@gmail.com>
2020-12-24 14:03:38 +0900

    re

--- db/seeds.rb
+++ db/seeds.rb
@@ -80,6 +80,7 @@ Book.create!(
 Book.last.book_locale_statuses.create!(
   locale_id: 2,
   is_main: true
+  difficulty: ['Unknown', 'Beginner', 'Intermediate','Advanced'].sample
 )
 
 content = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, “and what is the use of a book,” thought Alice “without pictures or conversations?”
