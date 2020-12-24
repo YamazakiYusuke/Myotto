e29af97e22018b3dd000e1747895a0b6a55d60ec
YamazakiYusuke <yusuke38098@gmail.com>
2020-12-24 17:43:18 +0900

    re

--- app/controllers/user_translation_comments_controller.rb
+++ app/controllers/user_translation_comments_controller.rb
@@ -1,18 +1,50 @@
 class UserTranslationCommentsController < ApplicationController
-  before_action :authenticate_user, only: [:create,]
-  before_action :authenticate_user, only: [:create]
-
+  before_action :set_translation, only: [:create, :edit, :update]
   def create
-    @comment = current_user.user_translation_comments.new(user_translation_comment_params)
-
-    if @comment.save
-      redirect_to translation_path(@comment.translation_id), notice: t('.commented')
-    else
-      redirect_to translation_path(@comment.translation_id), notice: t('.file_comment')
+    @comment = @translation.user_translation_comments.build(comment_params)
+    @comment[:user_id] = current_user.id
+    respond_to do |format|
+      if @comment.save
+        format.js { render :index }
+      else
+        format.html { redirect_to translation_path(@translation), notice: '投稿できませんでした...' }
+      end
+    end
+  end
+  def edit
+    @comment = @translation.user_translation_comments.find(params[:id])
+    respond_to do |format|
+      binding.pry
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+  def update
+    @comment = @translation.user_translation_comments.find(params[:id])
+    @comment[:user_id] = current_user.id
+    respond_to do |format|
+      if @comment.update(comment_params)
+        flash.now[:notice] = 'コメントが編集されました'
+        format.js { render :index }
+      else
+        flash.now[:notice] = 'コメントの編集に失敗しました'
+        format.js { render :edit_error }
+      end
+    end
+  end
+  def destroy
+    @comment = UserTranslationComment.find(params[:id])
+    @comment.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'コメントが削除されました'
+      format.js { render :index }
     end
   end
   private
-  def user_translation_comment_params
-    params.require(:user_translation_comment).permit(:content, :translation_id)
+  def comment_params
+    params.require(:user_translation_comment).permit(:translation_id, :content)
+  end
+  def set_translation
+    @translation = Translation.find(params[:translation_id])
   end
 end
