ec6958512ef1156b1b9477967fd98e0340620422
YamazakiYusuke <yusuke38098@gmail.com>
2021-04-26 19:59:03 +0900

    init unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '54.95.98.16', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/home/vagrant/.ssh/id_rsa'
+server '3.115.177.124', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '~/home/vagrant/.ssh/id_rsa'
